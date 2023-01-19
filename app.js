8d0080d2b1724afbaef9809f26da182dd0c6f1bd
tofa93 <tofig.valiyev1993@gmail.com>
2023-01-19 02:26:04 +0100

    Removed useless piece of code

--- app.js
+++ app.js
@@ -149,6 +149,22 @@ function movieList(movie) {
 
 getPopular();
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 4000);
+}
+
 
 
 // $('#zero-config').DataTable({
